65b0afd46bce13ccb6389a910075b4beb128b691
MSalas95 <MSalas.095@Gmail.com>
2014-11-03 15:47:03 -0430

    Estrutura de la API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+	config.middleware.insert_before "ActionDispatch::Static", "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
 end
